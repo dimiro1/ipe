8def052b9dfa408122e1d0cb3091ab5df2f8dc59
claudemiro <dimiro1@gmail.com>
2015-01-17 08:21:23 -0300

    Added a command line banner.

--- main.go
+++ main.go
@@ -45,6 +45,15 @@ func main() {
 }
 
 func printBanner() {
+	fmt.Print("\033[36m")
+	fmt.Print(`
+██╗██████╗ ███████╗
+██║██╔══██╗██╔════╝
+██║██████╔╝█████╗  
+██║██╔═══╝ ██╔══╝  
+██║██║     ███████╗
+╚═╝╚═╝     ╚══════╝`)
+	fmt.Println("\033[0m")
 	fmt.Println("\033[32mWelcome to Ipê - Yet another Pusher server clone\033[0m")
 	fmt.Println("\033[33mBy: Claudemiro Alves Feitosa Neto <dimiro1@gmail.com>\033[0m")
 }
